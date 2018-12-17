49a5749370d486e275e699d00a3fc5a73617833f
sfrieds3 <sfrieds3@gmail.com>
2018-12-17 17:49:32 -0600

    beginnings of the game

---
+++ src/point.py
@@ -0,0 +1,8 @@
+from typing import NamedTuple
+
+class Point(NamedTuple):
+    x: str
+    y: int
+
+    def __repr__(self):
+        return "str(self.x) + str(self.y)"
--- src/ttc.py
+++ src/ttc.py
@@ -1,19 +1,30 @@
 from board import *
+from point import *
+
 
 class Ttc:
     """Tic-Tac-Toe game - main class"""
 
     def __init__(self):
-        board = Board(3)
-        board.print_board()
+        self.board = Board(3)
+        self.winner = False
+        self.run_ttc()
 
     def run_ttc(self):
-        # run game
-        pass
+        self.board.print_board()
+
+        # while not self.winner:
+        point = self.get_move()
+        print(point)
+
+    def get_move(self):
+        x: str = input("Please enter x: ")
+        y: int = input("Please enter y: ")
+        return Point(x=x, y=y)
 
     def make_move():
         return "made a move"
 
 if __name__ == "__main__":
     ttc = Ttc()
-    ttc.run_ttc()
+

d1ff4ac70ea4f2c745b51d49708c0ae54522846d
sfrieds3 <sfrieds3@gmail.com>
2018-12-17 18:02:00 -0600

    add player number to prompt

--- src/ttc.py
+++ src/ttc.py
@@ -11,13 +11,17 @@ class Ttc:
         self.run_ttc()
 
     def run_ttc(self):
+        """run TTC game"""
         self.board.print_board()
 
         # while not self.winner:
-        point = self.get_move()
+        current_player = 1
+        point = self.get_move(current_player)
         print(point)
 
-    def get_move(self):
+    def get_move(self, current_player):
+        """Get move from player"""
+        print("CURRENT PLAYER: %s" %(current_player))
         x: str = input("Please enter x: ")
         y: int = input("Please enter y: ")
         return Point(x=x, y=y)

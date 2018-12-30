0344a012afc718d46ba053cb24e232f2228ba673
sfrieds3 <sfrieds3@gmail.com>
2018-12-30 16:59:36 -0600

    update tests

--- test_ai.py
+++ test_ai.py
@@ -7,7 +7,6 @@ def test_find_horizontal_winner():
 
     aimee = Aimee('O')
 
-    # test if solution in 1,1
     solution_board = []
     solution_board.append([None, 'O', 'O'])
     solution_board.append([None, None, None])
@@ -15,31 +14,6 @@ def test_find_horizontal_winner():
 
     assert aimee.find_horizontal_winner(solution_board) == Point(column='a', row=1)
 
-    # test if solution in 1,2
-    solution_board = []
-    solution_board.append([None, None, None])
-    solution_board.append([None, 'O', 'O'])
-    solution_board.append([None, None, None])
-
-    assert aimee.find_horizontal_winner(solution_board) == Point(column='a', row=2)
-
-    # test if solution in 1,3
-    solution_board = []
-    solution_board.append([None, None, None])
-    solution_board.append([None, None, None])
-    solution_board.append([None, 'O', 'O'])
-
-    assert aimee.find_horizontal_winner(solution_board) == Point(column='a', row=3)
-
-    # test if solution in 2,1
-    solution_board = []
-    solution_board.append(['O', None, 'O'])
-    solution_board.append([None, None, None])
-    solution_board.append([None, None, None])
-
-    assert aimee.find_horizontal_winner(solution_board) == Point(column='b', row=1)
-
-    # test if solution in 2,2
     solution_board = []
     solution_board.append([None, None, None])
     solution_board.append(['O', None, 'O'])
@@ -47,31 +21,6 @@ def test_find_horizontal_winner():
 
     assert aimee.find_horizontal_winner(solution_board) == Point(column='b', row=2)
 
-    # test if solution in 2,3
-    solution_board = []
-    solution_board.append([None, None, None])
-    solution_board.append([None, None, None])
-    solution_board.append(['O', None, 'O'])
-
-    assert aimee.find_horizontal_winner(solution_board) == Point(column='b', row=3)
-
-    # test if solution in 3,1
-    solution_board = []
-    solution_board.append(['O', 'O', None])
-    solution_board.append([None, None, None])
-    solution_board.append([None, None, None])
-
-    assert aimee.find_horizontal_winner(solution_board) == Point(column='c', row=1)
-
-    # test if solution in 3,2
-    solution_board = []
-    solution_board.append([None, None, None])
-    solution_board.append(['O', 'O', None])
-    solution_board.append([None, None, None])
-
-    assert aimee.find_horizontal_winner(solution_board) == Point(column='c', row=2)
-
-    # test if solution in 3,3
     solution_board = []
     solution_board.append([None, None, None])
     solution_board.append([None, None, None])
@@ -91,87 +40,39 @@ def test_find_horizontal_winner():
 def test_find_winning_move():
     """Test Aimee find_winning_move method"""
 
-    # aimee = Aimee('O')
+    aimee = Aimee('O')
 
-    # # test if solution in 1,1
-    # solution_board = []
-    # solution_board.append([None, 'O', 'O'])
-    # solution_board.append([None, None, None])
-    # solution_board.append([None, None, None])
+    # test horizontal
+    solution_board = []
+    solution_board.append([None, None, None])
+    solution_board.append([None, 'O', 'O'])
+    solution_board.append([None, None, None])
 
-    # assert aimee.find_winning_move(solution_board) == Point(column='a', row=1)
+    assert aimee.find_winning_move(solution_board) == Point(column='a', row=2)
 
-    # # test if solution in 1,2
-    # solution_board = []
-    # solution_board.append([None, None, None])
-    # solution_board.append([None, 'O', 'O'])
-    # solution_board.append([None, None, None])
+    # test vertical
+    solution_board = []
+    solution_board.append([None, None, None])
+    solution_board.append([None, None, 'O'])
+    solution_board.append([None, None, 'O'])
 
-    # assert aimee.find_winning_move(solution_board) == Point(column='a', row=2)
+    assert aimee.find_winning_move(solution_board) == Point(column='c', row=1)
 
-    # # test if solution in 1,3
-    # solution_board = []
-    # solution_board.append([None, None, None])
-    # solution_board.append([None, None, None])
-    # solution_board.append([None, 'O', 'O'])
+    # test diagonal
+    solution_board = []
+    solution_board.append(['O', None, None])
+    solution_board.append([None, None, None])
+    solution_board.append([None, None, 'O'])
 
-    # assert aimee.find_winning_move(solution_board) == Point(column='a', row=3)
+    assert aimee.find_winning_move(solution_board) == Point(column='b', row=2)
 
-    # # test if solution in 2,1
-    # solution_board = []
-    # solution_board.append(['O', None, 'O'])
-    # solution_board.append([None, None, None])
-    # solution_board.append([None, None, None])
+    # test if no solution
+    no_solution_board = []
+    no_solution_board.append([None, None, None])
+    no_solution_board.append([None, None, None])
+    no_solution_board.append([None, None, None])
 
-    # assert aimee.find_winning_move(solution_board) == Point(column='b', row=1)
-
-    # # test if solution in 2,2
-    # solution_board = []
-    # solution_board.append([None, None, None])
-    # solution_board.append(['O', None, 'O'])
-    # solution_board.append([None, None, None])
-
-    # assert aimee.find_winning_move(solution_board) == Point(column='b', row=2)
-
-    # # test if solution in 2,3
-    # solution_board = []
-    # solution_board.append([None, None, None])
-    # solution_board.append([None, None, None])
-    # solution_board.append(['O', None, 'O'])
-
-    # assert aimee.find_winning_move(solution_board) == Point(column='b', row=3)
-
-    # # test if solution in 3,1
-    # solution_board = []
-    # solution_board.append(['O', 'O', None])
-    # solution_board.append([None, None, None])
-    # solution_board.append([None, None, None])
-
-    # assert aimee.find_winning_move(solution_board) == Point(column='c', row=1)
-
-    # # test if solution in 3,2
-    # solution_board = []
-    # solution_board.append([None, None, None])
-    # solution_board.append(['O', 'O', None])
-    # solution_board.append([None, None, None])
-
-    # assert aimee.find_winning_move(solution_board) == Point(column='c', row=2)
-
-    # # test if solution in 3,3
-    # solution_board = []
-    # solution_board.append([None, None, None])
-    # solution_board.append([None, None, None])
-    # solution_board.append(['O', 'O', None])
-
-    # assert aimee.find_winning_move(solution_board) == Point(column='c', row=3)
-
-    # # test if no solution
-    # no_solution_board = []
-    # no_solution_board.append([None, None, None])
-    # no_solution_board.append([None, None, None])
-    # no_solution_board.append([None, None, None])
-
-    # assert aimee.find_winning_move(no_solution_board) is None
+    assert aimee.find_winning_move(no_solution_board) is None
 
 
 def test_get_vertical_winner():
@@ -223,6 +124,27 @@ def test_find_diagonal_winner():
     solution_board.append([None, None, None])
 
     assert aimee.find_diagonal_winner(solution_board) == Point(column='c', row=3)
+
+    solution_board = []
+    solution_board.append([None, None, 'O'])
+    solution_board.append([None, 'O', None])
+    solution_board.append([None, None, None])
+
+    assert aimee.find_diagonal_winner(solution_board) == Point(column='a', row=3)
+
+    solution_board = []
+    solution_board.append([None, None, 'O'])
+    solution_board.append([None, None, None])
+    solution_board.append(['O', None, None])
+
+    assert aimee.find_diagonal_winner(solution_board) == Point(column='b', row=2)
+
+    solution_board = []
+    solution_board.append([None, None, None])
+    solution_board.append([None, None, None])
+    solution_board.append(['O', None, None])
+
+    assert aimee.find_diagonal_winner(solution_board) is None
 
 
 def test_get_move():
